92ff6893ee26a8efc01b10af3d63f771491d4767
stnc <samtunc@yahooo.com>
2025-02-22 17:19:19 -0700

    first

---
+++ proxyRepo/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	pac "proxy/app"
+)
+
+func main() {
+
+	nginxServer := pac.NginxServer()
+	appStatusURL := "/app/status"
+	createuserURL := "/create/user"
+
+	httpCode, body := nginxServer.HandleRequest(appStatusURL, "GET")
+	fmt.Printf("\nUrl: %s\nHttpCode: %d\nBody: %s\n", appStatusURL, httpCode, body)
+
+	httpCode, body = nginxServer.HandleRequest(appStatusURL, "GET")
+	fmt.Printf("\nUrl: %s\nHttpCode: %d\nBody: %s\n", appStatusURL, httpCode, body)
+
+	httpCode, body = nginxServer.HandleRequest(appStatusURL, "GET")
+	fmt.Printf("\nUrl: %s\nHttpCode: %d\nBody: %s\n", appStatusURL, httpCode, body)
+
+	httpCode, body = nginxServer.HandleRequest(createuserURL, "POST")
+	fmt.Printf("\nUrl: %s\nHttpCode: %d\nBody: %s\n", appStatusURL, httpCode, body)
+
+	httpCode, body = nginxServer.HandleRequest(createuserURL, "GET")
+	fmt.Printf("\nUrl: %s\nHttpCode: %d\nBody: %s\n", appStatusURL, httpCode, body)
+
+
+
+	
+	httpCode, body = nginxServer.HandleRequest(createuserURL, "GET")
+	fmt.Printf("\nUrl: %s\nHttpCode: %d\nBody: %s\n", appStatusURL, httpCode, body)
+
+
+
+
+	
+	httpCode, body = nginxServer.HandleRequest(createuserURL, "GET")
+	fmt.Printf("\nUrl: %s\nHttpCode: %d\nBody: %s\n", appStatusURL, httpCode, body)
+
+	
+	httpCode, body = nginxServer.HandleRequest(createuserURL, "GET")
+	fmt.Printf("\nUrl: %s\nHttpCode: %d\nBody: %s\n", appStatusURL, httpCode, body)
+
+	
+	httpCode, body = nginxServer.HandleRequest(createuserURL, "GET")
+	fmt.Printf("\nUrl: %s\nHttpCode: %d\nBody: %s\n", appStatusURL, httpCode, body)
+
+
+	
+	httpCode, body = nginxServer.HandleRequest(createuserURL, "GET")
+	fmt.Printf("\nUrl: %s\nHttpCode: %d\nBody: %s\n", appStatusURL, httpCode, body)
+}
